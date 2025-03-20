fe8a156cb4250bc9bbc0c2290247ef54886bde3b
RodrigoScott <scott_rona1@hotmail.com>
2025-03-20 11:39:32 -0600

    feat: :art: fix const on home

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff743cbc)),
         useMaterial3: true,
       ),
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
