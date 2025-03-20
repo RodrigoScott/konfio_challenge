11d56b905e494dc10d74e25991a45d5db831e09e
RodrigoScott <scott_rona1@hotmail.com>
2025-03-20 12:01:36 -0600

    feat: :sparkles: create appbar

---
+++ lib/src/presentation/home/widgets/custom_app_bar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar {
+  customApp(Size size) {
+    return PreferredSize(
+      preferredSize: const Size(double.infinity, kToolbarHeight),
+      child: AppBar(
+        title: Center(
+            child: Text(
+          'Dogs We Love',
+          style: TextStyle(
+              fontSize: size.width * .08,
+              fontWeight: FontWeight.w500,
+              color: const Color(0xff333333)),
+        )),
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 16.0),
+          child: IconButton(
+            icon: const Icon(Icons.arrow_back_ios),
+            onPressed: () {
+
+            },
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: IconButton(onPressed: () {}, icon: const Icon(Icons.delete)),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/presentation/home/widgets/widgets.dart
@@ -0,0 +1,1 @@
+export 'package:konfio_challenge/src/presentation/home/widgets/custom_app_bar.dart';
