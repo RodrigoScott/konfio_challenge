77ed4dcfaecbe1c3f9e9444ea50afa6cecabd94c
RodrigoScott <scott_rona1@hotmail.com>
2025-03-20 12:23:15 -0600

    fix: :art: fix pet service

--- lib/src/domain/services/pet_service/pet_service.dart
+++ lib/src/domain/services/pet_service/pet_service.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:konfio_challenge/src/domain/models/models.dart';
 
@@ -10,8 +8,7 @@ class PetService {
       Uri.parse('https://jsonblob.com/api/1352326525529284608'),
     );
     if (response.statusCode == 200) {
-      var body = jsonDecode(response.body);
-      petList = petModelFromJson(body);
+      petList = petModelFromJson(response.body);
     }
     return petList;
   }
